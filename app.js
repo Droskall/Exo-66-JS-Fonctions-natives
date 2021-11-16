d44e6dcce5d12528544b7bf3433a875b009e0aaa
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-16 13:27:16 +0100

    finish

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+let entier = document.getElementById("entier");
+let flottant = document.getElementById("flottant");
+let nonNumerique = document.getElementById("non-numerique");
+let random = document.getElementById("random");
+let trunc = document.getElementById("trunc");
+let textReplace = document.getElementById("replace");
+let operande = 10;
+
+entier.innerHTML *= operande;
+
+flottant.innerHTML = parseFloat(flottant.innerHTML * operande + parseInt(entier.innerHTML));
+
+if (isNaN(nonNumerique)){
+    nonNumerique.innerHTML = 0;
+}
+
+random.innerHTML = Math.trunc(Math.random() * 100);
+
+trunc.innerHTML = Math.trunc(trunc.innerHTML);
+
+textReplace.innerHTML = textReplace.innerHTML.replace('world', 'Olivier Damien');
+
 console.log(random());
+
